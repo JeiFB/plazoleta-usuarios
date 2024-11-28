5b98679e992128ec7511e7002801d66468abac04
JeiFB <fuentesjeifer@gmail.com>
2024-11-27 21:16:49 -0500

    Configuración inicial del microservicio Usuarios y ajustes en la base de datos

---
+++ src/test/java/com/plazadecomidas/usuarios/usuarios/MicroservicioUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plazadecomidas.usuarios.usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class MicroservicioUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
